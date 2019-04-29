29a559f57ee25b76ea4f4826670db7de269513fd
Talia Jeffrey <tajeffrey11@gmail.com>
2019-04-29 21:54:21 +0000

    draft 2-ajax attempt

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // JavaScript File
+/* global $ */
+
+$("button").click(function(){
+    var value=$(".search").val();
+
+$.ajax({
+    url:"https://api.soundcloud.com/tracks?q=beyone&clientid=5aa8e389ba4e24b6106af5159ab3e344",
+    method:"GET",
+    success: function(response) {
+        $(".gallery").html("<img src='"+response[0].title+"'>")
+       
+    }
+});
+
+});
